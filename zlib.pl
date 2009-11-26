2a458642ee4c2fd3d92328267b0c7f70d8544bdf
Daniel de Kok <me@danieldk.eu>
2009-11-26 14:09:59 +0100

    Add support for term compression, including constraint handling.

--- zlib.pl
+++ zlib.pl
@@ -23,7 +23,12 @@
 
 :- module(zlib,[ zlib_compress/2,
 		 zlib_compress/3,
-		 zlib_uncompress/3 ]).
+		 zlib_compress_term/3,
+		 zlib_compress_term/4,
+		 zlib_uncompress/3,
+		 zlib_uncompress_term/3 ]).
+
+:- use_module(library(charsio)).
 
 foreign_resource(zlib,[ zlib_compress,
 			zlib_compress_default,
@@ -34,3 +39,23 @@ foreign(zlib_compress,c,zlib_compress(+term,+integer,[-term])).
 foreign(zlib_uncompress,c,zlib_uncompress(+term,+integer,[-term])).
 
 :- load_foreign_resource(zlib).
+
+zlib_compress_term(Term,Compressed,Len) :-
+    zlib_compress_term(Term,-1,Compressed,Len).
+
+zlib_compress_term(Term,Level,Compressed,Len) :-
+    call_residue(copy_term(Term,TermCopy),Cons),
+    numbervars(TermCopy/Cons,0,_),
+    charsio:format_to_chars('~q .',[TermCopy/Cons],Chars),
+    length(Chars,Len),
+    zlib_compress(Chars,Level,Compressed).
+
+zlib_uncompress_term(Compressed,Len,Term) :-
+    zlib_uncompress(Compressed,Len,Chars),
+    charsio:read_from_chars(Chars,Term/Cons),
+    call_constraints(Cons).
+
+call_constraints([]).
+call_constraints([_-Call|Rest]) :-
+    call(Call),
+    call_constraints(Rest).
